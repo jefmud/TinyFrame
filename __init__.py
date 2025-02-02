e045f41e0bcdf10e114d76ca2b11eebc9ea15e2b
jefmud <jefmud@gmail.com>
2025-02-02 11:14:56 -0500

    Create __init__.py
    initial commit

---
+++ __init__.py
@@ -0,0 +1,5 @@
+# TinyFrame - my tiny webframework
+# (C) 2025 Jeff Muday
+# Technical Debt to the Pallets Project, Armin Ronacher, Marcel Helkamp,
+# Ben Darnell, and a tiny bit of AI at many others!
+from .core import TinyFrame, TinyFrameView
