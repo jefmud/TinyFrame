245058dedb541cb659fabacbf00bdb0ad2924a10
jefmud <jefmud@gmail.com>
2025-02-11 11:25:19 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,8 @@
-# TinyFrame - my tiny webframework
+# TinyFrame - my tiny web framework
 # (C) 2025 Jeff Muday
+# idea based on Minimus Framework by me... but
 # Technical Debt to the Pallets Project, Armin Ronacher, Marcel Helkamp,
 # Ben Darnell, and a tiny bit of AI at many others!
-from .core import TinyFrame, TinyFrameView
+# TinyFrame is a WSGI web framework much of the ideas come from 
+# Bottle, Flask, Pyramid, Paste, etc.
+from .core import TinyFrame, ClassView
